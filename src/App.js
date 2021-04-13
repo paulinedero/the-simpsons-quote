8f8d7039512e7106db092c8efb17f272be9ca7df
Pauline De Ro <pauline.dero@gmail.com>
2021-04-13 08:52:22 +0200

    add content about the simpsons + API simpsons quote

--- src/App.js
+++ src/App.js
@@ -1,23 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import QuoteCard from './components/QuoteCards';
+import axios from 'axios';
 
 function App() {
+  const [showSimpson, setShowSimpson] = React.useState(false);
+  function handleShowSimpsonClick() {
+    setShowSimpson(!showSimpson);
+  }
+
+  const [quoteList, setQuoteList] = React.useState([
+    {
+      quote:
+        "These are my only friends...grown-up nerds like Gore Vidal. And even he's kissed more boys than I ever will.",
+      character: 'Lisa Simpson',
+      image:
+        'https://cdn.glitch.com/3c3ffadc-3406-4440-bb95-d40ec8fcde72%2FLisaSimpson.png?1497567512083',
+      characterDirection: 'Right'
+    },
+  ]);
+
+  const getSimpsons = () => {
+    axios
+      .get('https://simpsons-quotes-api.herokuapp.com/quotes?count=5')
+      .then((response) => response.data)
+      .then((data) => {
+        setQuoteList(data);
+      });
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <button onClick={handleShowSimpsonClick}>
+        Simpsons family only : {showSimpson ? 'ON' : 'OFF'}
+      </button>
+      <button type="button" onClick={getSimpsons}>Get Simpsons</button>
+      <h2>Simpsons' Characters</h2>
+      <div>
+        {quoteList
+          .filter(
+            (quote) => !showSimpson || quote.character.includes('Simpson')
+          )
+          .map((quote, index) => (
+            <QuoteCard key={index} {...quote} />
+          ))}
+      </div>
     </div>
   );
 }
